e455fbc85fde10cfd69176534d666b720864bcc6
jameseclement <jameseclement@gmail.com>
2018-12-12 01:15:51 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+//*for ([initialization]; [condition]; [iteration]) {
+  //[loopbody];
+// } 
+
+
+function forLoop(array){
+    var myarray = []
+    for (let i = 0; i < 25; i++){
+      
+    
+  }
+}
+  
+}
