b47c50b1d80fd70f8a339675e8b097411064d8e9
jameseclement <jameseclement@gmail.com>
2018-12-12 00:13:20 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,18 @@
+//*for ([initialization]; [condition]; [iteration]) {
+  //[loopbody];
+// } 
+
+for (var i = 1; i < 100; i++) {
+  console.log("Hello World the "+ i +" time");
+}
+
+function forLoop(array){
+  var array = {}
+  for (let i = 0; i < 25; i++){
+    if (i = 1) {
+    array.unshift("I am 1 strange loop")
+  } else {array.unshift("I am ${i} strange loops.")
+  }
+}
+  
+}
